15147cd9bb66ec61ba67578da0b6302d67080e49
Dylan Goncalves <dylanphx93@gmail.com>
2026-07-30 01:35:27 +0200

    style(competitions): fix code formatting and imports via Laravel Pint

--- app/Http/Controllers/CompetitionController.php
+++ app/Http/Controllers/CompetitionController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Competition;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use App\Models\Competition; 
-use Illuminate\Contracts\View\View; 
-use Illuminate\Http\RedirectResponse; 
 
 class CompetitionController extends Controller
 {
