0365ad38302ea6c45371dab43db7a031981ac737
Dylan Goncalves <dylanphx93@gmail.com>
2026-07-30 01:44:20 +0200

    fix(competitions): change the model name from competition to Competition

--- app/Models/competition.php
+++ app/Models/competition.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class competition extends Model
+class Competition extends Model
 {
     protected $fillable = ['name', 'description', 'status', 'start_date', 'end_date'];
 }
