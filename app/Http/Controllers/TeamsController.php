bbfb7850ed834de7b4cae7819438d9636c942e26
Dylan Goncalves <dylanphx93@gmail.com>
2026-07-30 10:53:27 +0200

    fix(teams): add missing namespace imports in the TeamController

--- app/Http/Controllers/TeamsController.php
+++ app/Http/Controllers/TeamsController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Team;
+use App\Models\SportMatch;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class TeamsController extends Controller
 {
